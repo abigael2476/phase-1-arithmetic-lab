90671fd80218d5d420f76870ac98b05bf91e849f
abigaelseenoi <abigaelseenoi5@gmail.com>
2025-09-08 17:53:07 +0300

    multiplied

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
-
 // Write your code here
+const num1 = 2;
+const num2 = 31;
+const num3 = 16;
+const num4 = 6;
+
+const multiply = num1 * num2;
+const random = Math.floor(Math.random() * 1000) + 1;
+const mod = num3 % num4;
+const max = Math.max(1, 2, 5, 10, 15, 20);
+
+module.exports = { num1, num2, num3, num4, multiply, random, mod, max };
